5cdd95301cfaa6f9a984e10f319ebde7869e53c8
KeerthanaKesavan11 <keerthana.kesavan@emishealth.com>
2025-03-03 17:42:46 +0530

    fix: multiple insert statements to single statement merge

--- src/Database/Migrations/V1_007__Insert_Course_Data.sql
+++ src/Database/Migrations/V1_007__Insert_Course_Data.sql
@@ -1,6 +1,7 @@
-INSERT INTO course (course_name,duration,fees) Values ('C','3 months',2000);
-INSERT INTO course (course_name,duration,fees) Values ('C++','4 months',3000);
-INSERT INTO course (course_name,duration,fees) Values ('C#','6 months',10000);
-INSERT INTO course (course_name,duration,fees) Values ('Java','6 months',15000);
-INSERT INTO course (course_name,duration,fees) Values ('Python','5 months',8000);
-INSERT INTO course (course_name,duration,fees) Values ('AWS','2 months',5000);
+INSERT INTO course (course_name,duration,fees) Values 
+('C','3 months',2000),
+('C++','4 months',3000),
+('C#','6 months',10000),
+('Java','6 months',15000),
+('Python','5 months',8000),
+('AWS','2 months',5000);
--- src/Database/Migrations/V1_010__Insert_Fees_Detail_Data.sql
+++ src/Database/Migrations/V1_010__Insert_Fees_Detail_Data.sql
@@ -1,9 +1,10 @@
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (1,10000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (2,8000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (3,5000,'NOT PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (4,5000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (5,10000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (6,15000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (7,3000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (8,8000,'PAID');
-Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values (9,3000,'NOT PAID');
+Insert into fees_detail (enrollment_id,amount_paid,payment_status) Values 
+(1,10000,'PAID'),
+(2,8000,'PAID'),
+(3,5000,'NOT PAID'),
+(4,5000,'PAID'),
+(5,10000,'PAID'),
+(6,15000,'PAID'),
+(7,3000,'PAID'),
+(8,8000,'PAID'),
+(9,3000,'NOT PAID');
